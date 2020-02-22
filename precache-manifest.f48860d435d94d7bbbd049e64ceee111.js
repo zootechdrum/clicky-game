705006dcb0232d06f204a014ba29e00f9b03824b
Cesar Gomez <zootechdrum@gmail.com>
2020-02-22 11:24:08 -0800

    Updates

---
+++ precache-manifest.f48860d435d94d7bbbd049e64ceee111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31aac6104703f517938e3a9d610e65bd",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "a2f4ad6e4dcac6cc9f65",
+    "url": "/clicky-game/static/css/main.180ce10d.chunk.css"
+  },
+  {
+    "revision": "e78143b64671d46574ad",
+    "url": "/clicky-game/static/js/2.7dc1971e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.7dc1971e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2f4ad6e4dcac6cc9f65",
+    "url": "/clicky-game/static/js/main.76f20f14.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
